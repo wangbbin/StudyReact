ed45be2ca555e5dd461a0478ae1eca1a94907f29
wangbbin <wbb499477096@qq.com>
2016-09-27 23:20:47 +0800

    3 about browserify & webpack CLI

--- react.js
+++ react.js
@@ -3,3 +3,13 @@ var ReactDOMServer = require('react-dom/server');
 
 var element = React.createElement('div', null, 'Hello World!');
 console.log(ReactDOMServer.renderToString(element));
+
+
+/*To install React DOM and build your bundle with browserify:
+
+$ npm install --save react react-dom babelify babel-preset-react
+$ browserify -t [ babelify ] main.js -o bundle.js
+To install React DOM and build your bundle with webpack:
+
+$ npm install --save react react-dom babel-preset-react babel-loader babel-core
+$ webpack main.js bundle.js --module-bind 'js=babel-loader'*/
